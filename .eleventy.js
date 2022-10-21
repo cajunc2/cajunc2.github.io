6f90ea334a66ad89fb83ec585995fbdffa7df82f
Matthew Martin <cajunc2@gmail.com>
2022-10-21 14:22:33 -0400

    Reworked project structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("css");
+	return {
+		dir: {
+			input: "src",
+			output: "docs"
+		}
+	}
 };
