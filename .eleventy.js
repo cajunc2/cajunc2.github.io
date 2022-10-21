2f5b8d0399241d57fdc4e5225e485fd4401f1b94
Matthew Martin <cajunc2@gmail.com>
2022-10-21 14:23:47 -0400

    Added .nojekyll to passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addPassthroughCopy(".nojekyll");
 	return {
 		dir: {
 			input: "src",
